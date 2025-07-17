9fbd500cbab5b961eb4e52e196805debe38ca512
Jose <142075563+BirdRoad1@users.noreply.github.com>
2025-07-17 11:52:17 -0400

    basic frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
+import styles from "./css/app.module.css";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className={styles.content}>
+      <h1>GroupStudy</h1>
+      <div className={styles.btns}>
+        <button>Create Room</button>
+        <button>Join Room</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
